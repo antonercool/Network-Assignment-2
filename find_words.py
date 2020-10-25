e2aa2e37d0a2c7e7b91118e316435ad855e472b7
antonercool <antonsihm@gmail.com>
2020-10-25 20:42:24 +0100

    added more guesses

--- find_words.py
+++ find_words.py
@@ -16,9 +16,7 @@ def x_or_chipers():
 
     return xor_content
 
-def guess_word1():
-
-    guess_word1 = "for people around the world with"
+def guess_word1(guess_word1):
     guess_word1_as_bytes = guess_word1.encode("ISO-8859-1")
     try_word(guess_word1_as_bytes)
     
@@ -31,7 +29,6 @@ def try_word(guess_word_as_bytes):
             byte_array_readable_output[i] = guess_word_as_bytes[i] ^ byte_array_xor[i+slide_len]       
         readable_output_as_string = byte_array_readable_output.decode("ISO-8859-1")
         print("slide = " , slide_len , "readable string = " , readable_output_as_string)
-
 
 def print_list():
     cracked_words = open("cracked.txt", "r")
@@ -52,15 +49,61 @@ def print_list():
         
 if __name__ == "__main__":
     #print(x_or_chipers())
-    #guess_word1()
+    guess_word1(" or ")
     print_list()
+
+# next -->  , rule
 
 # Word list
 # Whe  61, As, 128, this 142, " has a" internet 70
 
+# " this"  142  guess " the "
+# "has a"  127  guess " the "
+# "freed"  82   guess " the " 
+# "ence"   22   guess " the "
+
+# " of,"  142   guess " of "
+# " net"  52    guess " of "
+# "rude"  46    guess " of "
+# "oelt"  12    guess " of "
+
+# "them"  61    guess "them"
+
+# "hate" 127    guess " to "    -->  " to a "
+# "end"  22     guess " to "
+
+# " as " 143    guess  " as "
+
+# " in," 142    guess  " in "
+# "icur" 109    guess  " in "
+# "inhi" 106    guess  " in "
+# "eksb" 99     guess  " in "
+# "foc " 82     guess  " in "
+# "eft " 49     guess  " in "
+# "rsle" 46     guess  " in "
+# "ese "  22    guess  " in "
+# "ocdt" 12     guess  " in "
+
+# " is,"        guess  " is "
+
+# "rule"        guess  " on "
+
 #english 141
 # " around the world" 121  - guess "ilt a system that has as its goal to"
 # "m that has" 110 - guess " for people around the world with"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #def string_to_hex(str):
