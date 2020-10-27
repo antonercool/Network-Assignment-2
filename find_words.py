4c21cf87ea32be3279701e2bccb8830405fd9d1f
antonercool <antonsihm@gmail.com>
2020-10-27 11:57:06 +0100

    clean up

--- find_words.py
+++ find_words.py
@@ -19,13 +19,14 @@ def x_or_chipers():
     for i in range(200):
         xor_content[i] = content1[i] ^ content2[i]
 
-
     return xor_content
+
 
 def guess_word1(guess_word1):
     guess_word1_as_bytes = guess_word1.encode("ISO-8859-1")
     try_word(guess_word1_as_bytes)
-    
+
+
 def try_word(guess_word_as_bytes):
     byte_array_xor = x_or_chipers()
     byte_array_readable_output = bytearray(len(guess_word_as_bytes))
@@ -36,6 +37,7 @@ def try_word(guess_word_as_bytes):
         readable_output_as_string = byte_array_readable_output.decode("ISO-8859-1")
         if re.match(sampleRegex, readable_output_as_string):
             print("slide = " , slide_len , "readable string = " , readable_output_as_string)        
+
 
 def print_list():
     cracked_words = open("cracked.txt", "r")
@@ -54,6 +56,7 @@ def print_list():
     for char in c2chars:
         print(char, end = '')
 
+
 def fetch_c1():
     cracked_words = open("cracked.txt", "r")
     c1chars = []
@@ -62,6 +65,7 @@ def fetch_c1():
        c1chars.append(line[4])
        i = i + 1
     return c1chars
+
 
 def fetch_c2():
     cracked_words = open("cracked.txt", "r")
@@ -74,6 +78,7 @@ def fetch_c2():
     c2chars.pop()
     c2chars.pop()
     return c2chars
+
 
 def sha256sum():
     cracked_plainText_c1 = listToString(fetch_c1())
@@ -94,7 +99,7 @@ def sha256sum():
   
 
 # Calculate the encryption key by brute force
-# Give the plaintext and the cypher, we cant to find the key
+# Give the plaintext and the cypher, we can to find the key
 def calc_encryption_key():
     chipher1 = open("challenge1.txt", "rb") # 203 bytes = 203 keys 
     plainText = fetch_c1()
@@ -130,6 +135,7 @@ def listToString(s):
     # return string   
     return str1
 
+
 if __name__ == "__main__":
     #print(x_or_chipers())
     #word = sys.argv[1]
@@ -140,7 +146,3 @@ if __name__ == "__main__":
     sha256sum()
     #cypher_keys = calc_encryption_key()
     #plainText = fetch_c1()
-    #lort = encrypt_otp(cypher_keys, plainText)
-    #tis = encrypt_otp(cypher_keys, lort)
-    #print(lort)
-    #print(tis)
