4d0c4fc06bc880ae316003e184416ef4a4217b9e
Whizel <viktor.soendergaard@hotmail.com>
2020-11-01 17:04:36 +0100

    new testing hardcoded p,q

--- rabin.py
+++ rabin.py
@@ -36,13 +36,9 @@ Then convert it to binary and extend the binary value with itself,
 3. Encrypt with the formula:C = m2 mod n
 4. Send C to recipient. """
 def encrypt_message(public_key, message):
-    res = ''.join(format(ord(i), 'b') for i in message) 
-    return (int(res, 2)**2) % public_key
-
-def new_encrypt_message(public_key, message):
     array2 = bytes(message, 'ascii')
     int_value = int.from_bytes(array2, byteorder='little', signed=True)
-    return int_value**2 % public_key
+    return (int_value**2) % public_key
 
 def egcd(a,b):   
     s1, s2 = 1, 0   
@@ -51,9 +47,9 @@ def egcd(a,b):
         q = a//b    
         r = a%b   
         a, b = b, r     
-        s = s1-q*s2    
+        s = s1 - (q*s2)    
         s1, s2 = s2, s      
-        t = t1-q*t2    
+        t = t1 - (q*t2)    
         t1, t2 = t2, t    
     return (s1, t1)
 
@@ -64,12 +60,12 @@ def decrypt_message(encrypted_message, p, q, n):
     yp = egcd_tuple[0]
     yq = egcd_tuple[1]
     #Compute the square root of c modulo p and q
-    mp = fast_pow(encrypted_message, (p + 1) // 4, p)
-    mq = fast_pow(encrypted_message, (q + 1) // 4, q)
+    mp = fast_pow(encrypted_message, (p + 1) / 4, p)
+    mq = fast_pow(encrypted_message, (q + 1) / 4, q)
     #Chinese remainder theorem 
     r1 = (yp * p * mq + yq * q * mp) % n
     r2 = n - r1
-    r3 = (yp * p * mq + yq * q * mp) % n
+    r3 = (yp * p * mq - yq * q * mp) % n
     r4 = n - r3
     return r1, r2 , r3, r4
     #print('This is a:', a, "\n" , "This is b:", b, "\n", "This is r:", r, "\n", "This is s:", s)
@@ -78,16 +74,18 @@ if __name__ == '__main__':
     message = "hello world"
     bit = 256
     primetuple = generate_primes(bit)
-    p = primetuple[0]
-    q = primetuple[1]
+    #p = primetuple[0]
+    p = 75836544604891967003256481944881951354324450359384424162388676647823973947099
+    #q = primetuple[1]
+    q = 88672345796111985919470355381854441396924885416511841624542279453397353233707
     n = generate_public_key(p, q)
     encrypted_message = encrypt_message(n, message)
-    new_encrypted_message = new_encrypt_message(n, message)
-    #print("this is old; ",encrypted_message, "\n", "This is new; ",new_encrypted_message)
     decrypt_tuple = decrypt_message(encrypted_message, p, q, n)
     for x in decrypt_tuple:
         bytestest = x.to_bytes(100, byteorder='little')
         print(str(bytestest, 'ISO-8859-1'), "\n")
+
+
         
 
 
