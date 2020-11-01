b7db907a9d3c96bd984fb8377096a18b5bd001d9
Whizel <viktor.soendergaard@hotmail.com>
2020-11-01 17:37:49 +0100

    Changed around a lot added proper encryption

--- rabin.py
+++ rabin.py
@@ -3,7 +3,59 @@
 # Large Prime Generation for RSA 
 #https://www.geeksforgeeks.org/rabin-cryptosystem-with-implementation/
 from Cryptodome.Util import number
-  
+
+""" Encryption
+1. Get the public key n.
+2. Convert the message to ASCII value. 
+Then convert it to binary and extend the binary value with itself,
+ and change the binary value back to decimal m.
+3. Encrypt with the formula:C = m2 mod n
+4. Send C to recipient. """
+def encryption(public_key, message):
+    #array2 = bytes(message, 'ascii')
+    #int_value = int.from_bytes(array2, byteorder='little', signed=True)
+    message_padded = padding(message)
+    return (message_padded**2) % public_key  
+
+def padding(message):
+    binary_str = bin(message)     # convert to a bit string
+    output = binary_str + binary_str[-16:]      # pad the last 16 bits to the end
+    return int(output, 2)       # convert back to integer
+
+def decryption(encrypted_message, p, q, n):
+    egcd_tuple = egcd(p, q)
+    # Find yp and yq with extended Eucildean algorithm
+    yp = egcd_tuple[0]
+    yq = egcd_tuple[1]
+    #Compute the square root of c modulo p and q
+    mp = fast_pow(encrypted_message, (p + 1) / 4, p)
+    mq = fast_pow(encrypted_message, (q + 1) / 4, q)
+    #Chinese remainder theorem 
+    lst = [r1, r2, r3, r4]
+    r1 = (yp * p * mq + yq * q * mp) % n
+    r2 = n - r1
+    r3 = (yp * p * mq - yq * q * mp) % n
+    r4 = n - r3
+    return r1, r2 , r3, r4
+
+    plaintext = choose(lst)
+    string = bin(plaintext)
+    string = string[:-16]
+    plaintext = int(string, 2)
+
+    return plaintext
+    #print('This is a:', a, "\n" , "This is b:", b, "\n", "This is r:", r, "\n", "This is s:", s)
+
+def choose(lst):
+    for i in lst:
+        binary = bin(i)
+
+        append = binary[-16:]   # take the last 16 bits
+        binary = binary[:-16]   # remove the last 16 bits
+
+        if append == binary[-16:]:
+            return i
+    return
 
 def fast_pow(num, power, mod):
     #result
@@ -28,17 +80,7 @@ def generate_primes(bitsize):
 def generate_public_key(p, q):
     return p * q
 
-""" Encryption
-1. Get the public key n.
-2. Convert the message to ASCII value. 
-Then convert it to binary and extend the binary value with itself,
- and change the binary value back to decimal m.
-3. Encrypt with the formula:C = m2 mod n
-4. Send C to recipient. """
-def encrypt_message(public_key, message):
-    array2 = bytes(message, 'ascii')
-    int_value = int.from_bytes(array2, byteorder='little', signed=True)
-    return (int_value**2) % public_key
+
 
 def egcd(a,b):   
     s1, s2 = 1, 0   
@@ -54,33 +96,22 @@ def egcd(a,b):
     return (s1, t1)
 
 
-def decrypt_message(encrypted_message, p, q, n):
-    egcd_tuple = egcd(p, q)
-    # Find yp and yq with extended Eucildean algorithm
-    yp = egcd_tuple[0]
-    yq = egcd_tuple[1]
-    #Compute the square root of c modulo p and q
-    mp = fast_pow(encrypted_message, (p + 1) / 4, p)
-    mq = fast_pow(encrypted_message, (q + 1) / 4, q)
-    #Chinese remainder theorem 
-    r1 = (yp * p * mq + yq * q * mp) % n
-    r2 = n - r1
-    r3 = (yp * p * mq - yq * q * mp) % n
-    r4 = n - r3
-    return r1, r2 , r3, r4
-    #print('This is a:', a, "\n" , "This is b:", b, "\n", "This is r:", r, "\n", "This is s:", s)
+
 
 if __name__ == '__main__': 
-    message = "hello world"
+    message = "hello"
+    plaintext =  number.bytes_to_long(message.encode('utf-8'))
     bit = 256
     primetuple = generate_primes(bit)
     #p = primetuple[0]
-    p = 75836544604891967003256481944881951354324450359384424162388676647823973947099
+    p = 270576397968349702240268570806789986947
     #q = primetuple[1]
-    q = 88672345796111985919470355381854441396924885416511841624542279453397353233707
-    n = generate_public_key(p, q)
-    encrypted_message = encrypt_message(n, message)
-    decrypt_tuple = decrypt_message(encrypted_message, p, q, n)
+    q = 219242991039694982252977037582947245671
+    #n = generate_public_key(p, q)
+    n = 59321978795327837368543975705006433832257716767865371948246597234695692256437
+    encrypted_message = encryption(n, plaintext)
+    print(encrypted_message)
+    decrypt_tuple = decryption(encrypted_message, p, q, n)
     for x in decrypt_tuple:
         bytestest = x.to_bytes(100, byteorder='little')
         print(str(bytestest, 'ISO-8859-1'), "\n")
