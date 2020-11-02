5fc3ce1b3e8900c03d7f00314a6ef135ccc6e6b0
antonercool <antonsihm@gmail.com>
2020-11-02 12:32:20 +0100

    Fixed bug + added padding + added fecthing correct + added decode + added encode

--- rabin.py
+++ rabin.py
@@ -3,42 +3,79 @@
 # Large Prime Generation for RSA 
 #https://www.geeksforgeeks.org/rabin-cryptosystem-with-implementation/
 from Cryptodome.Util import number
+from Crypto import Random
+import Crypto
+import codecs
+import sys
   
-
-def fast_pow(num, power, mod):
-    #result
-    res = 1 
-    # (b ^ k) * p = num ^ power
-    while power > 0:
-        while power % 2 == 0:
-            power /= 2
-            num = (num * num) % mod
-        power -= 1
-        res = (res * num) % mod
-    return res   
-
 #Generate primes using Cryptodome
 def generate_primes(bitsize):
-    p = number.getPrime(bitsize)
     while True:
-        q = number.getPrime(bitsize)
-        if (p != q):
+        p = number.getPrime(bitsize, randfunc=Crypto.Random.get_random_bytes)
+        q = number.getPrime(bitsize, randfunc=Crypto.Random.get_random_bytes)
+        ## Make sure the big primes are not equal + ensure 
+        if (p != q) and ((p % 4) ==3) and ((q % 4)==3):
             return (p,q)
 
 def generate_public_key(p, q):
     return p * q
 
-""" Encryption
-1. Get the public key n.
-2. Convert the message to ASCII value. 
-Then convert it to binary and extend the binary value with itself,
- and change the binary value back to decimal m.
-3. Encrypt with the formula:C = m2 mod n
-4. Send C to recipient. """
 def encrypt_message(public_key, message):
-    array2 = bytes(message, 'ascii')
-    int_value = int.from_bytes(array2, byteorder='little', signed=True)
-    return (int_value**2) % public_key
+    return message**2 % public_key      
+
+def encode_message(message):
+    hex_acum = "0x"
+    # do twice for padding
+    for i in range(2):
+        for charactor in message:
+            hex_acum += str(hex(ord(charactor)))[2:]
+
+    message_as_value = int(hex_acum, 16)
+    return message_as_value
+
+
+def is_correct_message(message_as_value):
+    message_as_bytes = int_to_bytes(message_as_value)
+    message_size = int(len(message_as_bytes)/2)
+    is_corret = True 
+    for i in range(message_size):
+        if message_as_bytes[i] == message_as_bytes[i+message_size] :
+            is_corret &= True
+        else:
+            is_corret &= False
+
+    return is_corret
+
+def fetch_correct_message(decrypted_tuple):
+    for message in decrypted_tuple:
+        if is_correct_message(message):
+            return message
+
+
+# https://stackoverflow.com/questions/21017698/converting-int-to-bytes-in-python-3
+def int_to_bytes(x: int) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+
+def decode_message(value):
+    string_acum = ""
+    value_as_hex = codecs.encode(int_to_bytes(value), "hex").decode("utf-8")  
+    raw_bytes = bytes.fromhex(value_as_hex)
+    for i in range(int(len(raw_bytes)/2)):
+        string_acum += chr(raw_bytes[i])
+
+    return string_acum
+
+# https://www.geeksforgeeks.org/python-program-for-basic-and-extended-euclidean-algorithms-2/
+def gcd_extended(a, b):  
+    # Base Case  
+    if a == 0 :   
+        return b,0,1
+    gcd,x1,y1 = gcd_extended(b%a, a)  
+    x = y1 - (b//a) * x1  
+    y = x1  
+     
+    return gcd,x,y 
 
 def egcd(a,b):   
     s1, s2 = 1, 0   
@@ -51,58 +88,55 @@ def egcd(a,b):
         s1, s2 = s2, s      
         t = t1 - (q*t2)    
         t1, t2 = t2, t    
-    return (s1, t1)
-
+    return (s1, t1)    
+      
 
 def decrypt_message(encrypted_message, p, q, n):
-    egcd_tuple = egcd(p, q)
     # Find yp and yq with extended Eucildean algorithm
+    egcd_tuple = egcd(p, q)
     yp = egcd_tuple[0]
     yq = egcd_tuple[1]
     #Compute the square root of c modulo p and q
-    mp = fast_pow(encrypted_message, (p + 1) / 4, p)
-    mq = fast_pow(encrypted_message, (q + 1) / 4, q)
+    if (yp*p+yq*q) != 1:
+        print("ERROR!")
+    mp = pow(encrypted_message, (p + 1) // 4, p)
+    mq = pow(encrypted_message, (q + 1) // 4, q)
+
     #Chinese remainder theorem 
     r1 = (yp * p * mq + yq * q * mp) % n
     r2 = n - r1
     r3 = (yp * p * mq - yq * q * mp) % n
     r4 = n - r3
     return r1, r2 , r3, r4
-    #print('This is a:', a, "\n" , "This is b:", b, "\n", "This is r:", r, "\n", "This is s:", s)
-
+  
 if __name__ == '__main__': 
-    message = "hello world"
-    bit = 256
-    primetuple = generate_primes(bit)
-    #p = primetuple[0]
-    p = 75836544604891967003256481944881951354324450359384424162388676647823973947099
-    #q = primetuple[1]
-    q = 88672345796111985919470355381854441396924885416511841624542279453397353233707
-    n = generate_public_key(p, q)
-    encrypted_message = encrypt_message(n, message)
-    decrypt_tuple = decrypt_message(encrypted_message, p, q, n)
-    for x in decrypt_tuple:
-        bytestest = x.to_bytes(100, byteorder='little')
-        print(str(bytestest, 'ISO-8859-1'), "\n")
+    message = sys.argv[1]
+    bit_size = 256
+    primetuple = generate_primes(bit_size)
+    
+    p = primetuple[0]
+    q = primetuple[1]
+    n = generate_public_key(q,p)
+    print("Keys for encryption : ")
+    print("\t p : " , p )
+    print("\t q : " , q )
+    print("\t n : " , n )
 
+    encoded_message = encode_message(message)
+    print("Preparing text for encrypting -- encoding")
+    print("Encoded messsage with padding :", encoded_message)
+    encrypted_message = encrypt_message(n, encoded_message)
+    
+    print("Encrypted messsage :" ,encrypted_message)
+    decrypted_message = decrypt_message(encrypted_message, p, q, n)
 
-        
+    i = 1
+    for cipher in decrypted_message:
+        print("decrypted chiper nr :", i , " = ", cipher) 
+        i = i + 1
 
+    print("\t fecthing correct decryptet cipher..")
+    
+    final_message = fetch_correct_message(decrypted_message)
+    print("final raw text : " ,decode_message(final_message) )
 
-""" Decryption
-1. Accept C from sender.
-2. Specify a and b with Extended Euclidean GCD such that, a.p + b.q = 1
-3. Compute r and s using following formula:
-r = C(p+1)/4 mod p
-s = C(q+1)/4 mod q
-4. Now, calculate X and Y using following formula:
-X = ( a.p.r + b.q.s ) mod p
-Y = ( a.p.r – b.q.s ) mod q
-5. The four roots are, m1=X, m2=-X, m3=Y, m4=-Y
-Now, Convert them to binary and divide them all in half.
-6. Determine in which the left and right half are same. Keep that binary’s one half
- and convert it to decimal m. Get the ASCII character 
- for the decimal value m. The resultant character gives the correct message sent by sender.
- """
-
- 
